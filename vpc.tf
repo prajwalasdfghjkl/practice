28abac10079966f7138c487a30d15ee88d4a366f
Prajwal Lamkhade <125536931+prajwalasdfghjkl@users.noreply.github.com>
2024-04-10 14:35:49 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,37 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
 resource "aws_vpc" "myvpc" {
-  cidr_block = "10.10.0.0/16"
+  cidr_block       = "10.10.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Myvpc"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.10.1.0/24"
+
+  tags = {
+    Name = "Public"
+  }
+}
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.10.2.0/24"
+
+  tags = {
+    Name = "Private"
+  }
+
+}
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.10.3.0/24"
+
+  tags = {
+    Name = "Private2"
+  }
+
 }
