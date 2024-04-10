ad262db8933bbd634e7785be0630029d8015e407
Prajwal Lamkhade <125536931+prajwalasdfghjkl@users.noreply.github.com>
2024-04-10 15:22:21 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -9,29 +9,3 @@ resource "aws_vpc" "myvpc" {
     Name = "Myvpc"
   }
 }
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.10.1.0/24"
-
-  tags = {
-    Name = "Public"
-  }
-}
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.10.2.0/24"
-
-  tags = {
-    Name = "Private"
-  }
-
-}
-resource "aws_subnet" "private1" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.10.3.0/24"
-
-  tags = {
-    Name = "Private2"
-  }
-
-}
